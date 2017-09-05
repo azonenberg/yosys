ea75173d3b69eb8750771023fc01fb45ec98e71b
Andrew Zonenberg <azonenberg@drawersteak.com>
2017-09-04 22:03:23 -0700

    Initial recover_tff_counters implementation. Far from all-inclusive but works for power-of-two counts.

--- passes/techmap/recover_tff_counters.cc
+++ passes/techmap/recover_tff_counters.cc
@@ -43,26 +43,36 @@ ModIndex::PortInfo GetDriverOfPort(Cell* cell, RTLIL::IdString port, RTLIL::IdSt
 void recover_tff_counters_worker(
 	ModIndex& index,
 	Cell *cell,
-	unsigned int& /*total_counters*/,
-	pool<Cell*>& /*cells_to_remove*/)
+	unsigned int& total_counters,
+	pool<Cell*>& cells_to_remove)
 {
 	SigMap& sigmap = index.sigmap;
+	auto module = cell->module;
 
 	//Start by looking at TFFs
-	if (cell->type.str().find("__TFF_") == string::npos)
+	if (cell->type.str().find("_DFF") == string::npos)
 		return;
 
-	//Find the driver of our T input. It should be a ANDNOT for all but the LSB, which is a NOR.
-	auto tdriver = GetDriverOfPort(cell, "\\T", "\\Y", index);
+	//Find the driver of our D input. It should be an inverter for the LSB, which is our anchor cell.
+	auto tdriver = GetDriverOfPort(cell, "\\D", "\\Y", index);
 	if(tdriver == ModIndex::PortInfo())
 		return;
-	if(tdriver.cell->type != "$_NOR_")
+	if(tdriver.cell->type != "$_NOT_")
 		return;
 
-	log("Found candidate counter anchored at %s (%s)\n", log_id(cell->name), cell->type.c_str());
+	//Verify that the inverter is driven by our output
+	auto anchordriver = GetDriverOfPort(tdriver.cell, "\\A", "\\Q", index);
+	if(anchordriver == ModIndex::PortInfo())
+		return;
+	if(anchordriver.cell != cell)
+		return;
+
+	//TODO: Figure out how to recover reset value etc
+
+	//log("Found candidate counter LSB at %s (%s)\n", log_id(cell->name), cell->type.c_str());
 
 	//Look down the counter toward the MSB.
-	//We should see a chain of ANDNOTs and TFFs (TFFs with the same reset polarity as us)
+	//We should see a chain of AND-NOTs and TFFs, sharing our reset and clock inputs
 	auto anchor_reset = index.sigmap(cell->getPort("\\R"));
 	auto anchor_clock = index.sigmap(cell->getPort("\\C"));
 	Cell* current_cell = cell;
@@ -78,16 +88,43 @@ void recover_tff_counters_worker(
 		std::vector<Cell*> andnots;
 		for(auto x : qports)
 		{
-			//Skip anything that isn't a ANDNOT
+			//Skip back edges
 			if(x.cell == current_cell)
 				continue;
-			if(x.cell->type != "$_ANDNOT_")
-				continue;
 
-			//The ANDNOT's port must be A or B (not Y)
-			//Y should never happen (multiple drivers) but check just in case
-			if(x.port == "\\Y")
-				continue;
+			//For the first element of the chain, there's just a NOT since the -1th element of the counter doesn't exist
+			if(downstream_flipflops.empty())
+			{
+				//Skip anything that isn't an inverter
+				if(x.cell->type != "$_NOT_")
+					continue;
+
+				//The inverter's port must be A (input)
+				if(x.port != "\\A")
+					continue;
+			}
+
+			//Otherwise, we expect a chain of AND-NOT cells
+			else
+			{
+				//Skip anything that isn't a ANDNOT
+				if(x.cell->type != "$_ANDNOT_")
+					continue;
+
+				//The ANDNOT's port must be B (the inverting output)
+				if(x.port != "\\B")
+					continue;
+
+				//The A input of the same ANDNOT cell must be our toggle input (the previous chain element's output)
+				auto noninv = GetDriverOfPort(x.cell, "\\A", "\\Y", index);
+				if(noninv == ModIndex::PortInfo())
+					continue;
+				auto expected_driver = GetDriverOfPort(current_cell, "\\T", "\\Y", index);
+				if(noninv.cell != expected_driver.cell)
+					continue;
+			}
+
+			//Match if we got this far
 			andnots.push_back(x.cell);
 		}
 
@@ -104,13 +141,11 @@ void recover_tff_counters_worker(
 			pool<ModIndex::PortInfo> yports = index.query_ports(y);
 			for(auto x : yports)
 			{
-				//Skip anything that isn't the same TFF type as us
+				//Make sure we didn't find a back edge!
 				if(x.cell == current_cell)
 					continue;
-				if(x.cell->type != current_cell->type)
-					continue;
 
-				//And we must be feeding into its T input
+				//We must be feeding into its T input
 				if(x.port != "\\T")
 					continue;
 
@@ -122,7 +157,7 @@ void recover_tff_counters_worker(
 
 				//Found it
 				current_cell = x.cell;
-				log("    Found flipflop in chain: %s\n", current_cell->name.c_str());
+				//log("    Found flipflop in chain: %s (%s)\n", current_cell->name.c_str(), current_cell->type.c_str());
 				hit = true;
 			}
 		}
@@ -130,210 +165,52 @@ void recover_tff_counters_worker(
 		if(!hit)
 			break;
 	}
-	log("  Found %zu downstream T flipflops\n", downstream_flipflops.size());
+	//log("  Found %zu downstream T flipflops\n", downstream_flipflops.size());
 
-	//Find the upstream flipflop (LSB)
-	//This should be a DFF connected to one of our tdriver's inputs.
-	auto tdriver_a = GetDriverOfPort(tdriver.cell, "\\A", "\\Q", index);
-	auto tdriver_b = GetDriverOfPort(tdriver.cell, "\\B", "\\Q", index);
-	auto type = cell->type.str().substr(6);		//"PP0_" or similar
-	log("  type=%s\n", type.c_str());
-	if(tdriver_a.cell)
-	{
-		auto ttype = tdriver_a.cell->type.str().substr(5);
-		log("  ttype=%s\n", ttype.c_str());
-		if( (tdriver_a.cell->type.str().find("$_DFF") == 0) && (ttype == type) )
-			log("  Hit on port A\n");
-	}
-	if(tdriver_b.cell)
-	{
-		auto ttype = tdriver_b.cell->type.str().substr(5);
-		log("  ttype=%s\n", ttype.c_str());
-		if( (tdriver_b.cell->type.str().find("$_DFF") == 0) && (ttype == type) )
-			log("  Hit on port B\n");
-	}
-
-	/*
-	pool<Cell*> rval;
-	for(auto b : port)
-	{
-		pool<ModIndex::PortInfo> ports = index.query_ports(b);
-		for(auto x : ports)
-		{
-			if(x.cell == src)
-				continue;
-			rval.insert(x.cell);
-		}
-	}
-	return rval;
-	*/
-
-	/*
-	//A input is the count value. Check if it has COUNT_EXTRACT set.
-	//If it's not a wire, don't even try
-	auto port = sigmap(cell->getPort("\\A"));
-	if(!port.is_wire())
-		return;
-	RTLIL::Wire* a_wire = port.as_wire();
-	bool force_extract = false;
-	bool never_extract = false;
-	string count_reg_src = a_wire->attributes["\\src"].decode_string().c_str();
-	if(a_wire->attributes.find("\\COUNT_EXTRACT") != a_wire->attributes.end())
-	{
-		pool<string> sa = a_wire->get_strpool_attribute("\\COUNT_EXTRACT");
-		string extract_value;
-		if(sa.size() >= 1)
-		{
-			extract_value = *sa.begin();
-			log("  Signal %s declared at %s has COUNT_EXTRACT = %s\n",
-				log_id(a_wire),
-				count_reg_src.c_str(),
-				extract_value.c_str());
-
-			if(extract_value == "FORCE")
-				force_extract = true;
-			else if(extract_value == "NO")
-				never_extract = true;
-			else if(extract_value == "AUTO")
-			{}	//default
-			else
-				log_error("  Illegal COUNT_EXTRACT value %s (must be one of FORCE, NO, AUTO)\n",
-					extract_value.c_str());
-		}
-	}
-
-	//If we're explicitly told not to extract, don't infer a counter
-	if(never_extract)
+	//If less than 3 bits long, don't extract
+	//TODO: make this configurable
+	int count_width = 1 + downstream_flipflops.size();
+	if(count_width < 3)
 		return;
 
-	//Attempt to extract a counter
-	CounterExtraction extract;
-	int reason = greenpak4_counters_tryextract(index, cell, extract);
+	log("  Converting T flipflops %s ... %s to a %d-bit down counter\n",
+		log_id(cell->name),
+		log_id(downstream_flipflops[downstream_flipflops.size()-1]->name),
+		count_width
+	);
 
-	//Nonzero code - we could not find a matchable counter.
-	//Do nothing, unless extraction was forced in which case give an error
-	if(reason != 0)
-	{
-		static const char* reasons[24]=
-		{
-			"no problem",									//0
-			"counter is larger than 14 bits",				//1
-			"counter does not count by one",				//2
-			"counter uses signed math",						//3
-			"counter does not count by one",				//4
-			"ALU is not a subtractor",						//5
-			"ALU is not a subtractor",						//6
-			"ALU ports used outside counter",				//7
-			"ALU ports used outside counter",				//8
-			"ALU output used outside counter",				//9
-			"ALU output is not a mux",						//10
-			"ALU output is not full bus",					//11
-			"Underflow value is not constant",				//12
-			"No underflow detector found",					//13
-			"Mux output is used outside counter",			//14
-			"Counter reg is not DFF/ADFF",					//15
-			"Counter input is not full bus",				//16
-			"Count register is used outside counter, but not by a DCMP or DAC",		//17
-			"Register output is not full bus",				//18
-			"Register output is not full bus",				//19
-			"No init value found",							//20
-			"Underflow value is not equal to init value",	//21
-			"Reset polarity is not positive",				//22
-			"Reset is not to zero"							//23
-		};
+	//TODO: do this!
+	log_warning("  Not copying INIT attributes from incoming TFFs\n");
+	log_warning("  Not checking set/reset polarity on original TFFs\n");
 
-		if(force_extract)
-		{
-			log_error(
-			"Counter extraction is set to FORCE on register %s, but a counter could not be inferred (%s)\n",
-			log_id(a_wire),
-			reasons[reason]);
-		}
-		return;
-	}
-
-	//Figure out the final cell type based on the counter size
-	string celltype = "\\GP_COUNT8";
-	if(extract.width > 8)
-		celltype = "\\GP_COUNT14";
-
-	//Get new cell name
-	string countname = string("$auto$GP_COUNTx$") + log_id(extract.rwire->name.str());
-
-	//Log it
+	//Update stats - we have a hit!
 	total_counters ++;
-	string reset_type = "non-resettable";
-	if(extract.has_reset)
-	{
-		//TODO: support other kind of reset
-		reset_type = "async resettable";
-	}
-	log("  Found %d-bit %s down counter %s (counting from %d) for register %s declared at %s\n",
-		extract.width,
-		reset_type.c_str(),
-		countname.c_str(),
-		extract.count_value,
-		log_id(extract.rwire->name),
-		count_reg_src.c_str());
 
-	//Wipe all of the old connections to the ALU
-	cell->unsetPort("\\A");
-	cell->unsetPort("\\B");
-	cell->unsetPort("\\BI");
-	cell->unsetPort("\\CI");
-	cell->unsetPort("\\CO");
-	cell->unsetPort("\\X");
-	cell->unsetPort("\\Y");
-	cell->unsetParam("\\A_SIGNED");
-	cell->unsetParam("\\A_WIDTH");
-	cell->unsetParam("\\B_SIGNED");
-	cell->unsetParam("\\B_WIDTH");
-	cell->unsetParam("\\Y_WIDTH");
+	//Create the new $_COUNT cell
+	auto counter = module->addCell(NEW_ID, "$__COUNT_");
+	counter->setParam("\\RESET_MODE", RTLIL::Const("FIXME"));	//not yet implemented
+	counter->setParam("\\WIDTH", RTLIL::Const(count_width));
+	counter->setParam("\\COUNT_TO", RTLIL::Const(pow(2, count_width) - 1);
+	counter->setParam("\\HAS_CE", RTLIL::Const(0));
+	counter->setParam("\\HAS_POUT", RTLIL::Const(1));
+	counter->setParam("\\DIRECTION", RTLIL::Const("DOWN"));
+	counter->setPort("\\CE", RTLIL::Const(1));
+	counter->setPort("\\UP", RTLIL::Const(0));
+	counter->setPort("\\RST", cell->getPort("\\R"));
+	counter->setPort("\\CLK", cell->getPort("\\C"));
+	//no OUT connection yet
 
-	//Change the cell type
-	cell->type = celltype;
-
-	//Hook up resets
-	if(extract.has_reset)
-	{
-		//TODO: support other kinds of reset
-		cell->setParam("\\RESET_MODE", RTLIL::Const("LEVEL"));
-		cell->setPort("\\RST", extract.rst);
-	}
-	else
-	{
-		cell->setParam("\\RESET_MODE", RTLIL::Const("RISING"));
-		cell->setPort("\\RST", RTLIL::SigSpec(false));
-	}
-
-	//Hook up other stuff
-	cell->setParam("\\CLKIN_DIVIDE", RTLIL::Const(1));
-	cell->setParam("\\COUNT_TO", RTLIL::Const(extract.count_value));
-
-	cell->setPort("\\CLK", extract.clk);
-	cell->setPort("\\OUT", extract.outsig);
-
-	//Hook up any parallel outputs
-	for(auto load : extract.pouts)
-	{
-		log("    Counter has parallel output to cell %s port %s\n", log_id(load.cell->name), log_id(load.port));
-
-		//Find the wire hooked to the old port
-		auto sig = load.cell->getPort(load.port);
-
-		//Connect it to our parallel output
-		//(this is OK to do more than once b/c they all go to the same place)
-		cell->setPort("\\POUT", sig);
-	}
+	//Drive POUT to the chain of TFFs
+	RTLIL::SigSpec outbus;
+	outbus.append(cell->getPort("\\Q"));
+	for(auto ff : downstream_flipflops)
+		outbus.append(ff->getPort("\\Q"));
+	counter->setPort("\\POUT", outbus);
 
 	//Delete the cells we've replaced (let opt_clean handle deleting the now-redundant wires)
-	cells_to_remove.insert(extract.count_mux);
-	cells_to_remove.insert(extract.count_reg);
-	cells_to_remove.insert(extract.underflow_inv);
-
-	//Finally, rename the cell
-	cells_to_rename.insert(pair<Cell*, string>(cell, countname));
-	*/
+	cells_to_remove.insert(cell);
+	for(auto ff : downstream_flipflops)
+		cells_to_remove.insert(ff);
 }
 
 struct RecoverTFFCountersPass : public Pass {
@@ -345,6 +222,8 @@ struct RecoverTFFCountersPass : public Pass {
 		log("    recover_tff_counters [selection]\n");
 		log("\n");
 		log("This pass converts chains of T flipflops into counters\n");
+		log("You should extract T flipflops by running \"recover_tff\" followed by\n");
+		log("mapping the design with \"abc -g ANDNOT\" before running this pass\n");
 		log("\n");
 	}
 	virtual void execute(std::vector<std::string> args, RTLIL::Design *design)
@@ -379,7 +258,7 @@ struct RecoverTFFCountersPass : public Pass {
 		}
 
 		if(total_counters)
-			log("Extracted %u counters\n", total_counters);
+			log("Extracted %u counter(s)\n", total_counters);
 	}
 } RecoverTFFCountersPass;
 
